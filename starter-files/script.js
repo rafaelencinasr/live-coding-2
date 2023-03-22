3f83d81a870c0f3f1c4e4392bb8601c961787c7b
Rafael Encinas <rafaeladalbertoencinas94@gmail.com>
2023-03-22 15:47:01 -0700

    Se agrego la solucion al ejercicio de live-coding-2 y un archivo de texto con la retroalimentacion recibida

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,6 +1,6 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
-
+console.log("test");
 /**
  * STEPS:
  *
@@ -11,3 +11,32 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+// Crear funcion para llamar a la API
+async function fetchAnswer(){
+    answerLocation.textContent="Loading...";
+    await fetch(API_ENDPOINT)
+    .then(res=>res.json())
+    .then(data=>{
+        answerLocation.textContent=data.answer;
+    });
+    
+    setTimeout(()=>{
+        borrarRespuesta()
+    },3000);
+}
+
+function borrarRespuesta(){
+    answerLocation.textContent="";
+}
+
+//Ubicacion de donde queremos mostrar la respuesta
+const answerLocation = document.querySelector("#answer");
+
+//Elemento del boton
+const buttonLocation = document.querySelector("#button");
+
+//Se agrega event listener al boton, cuando se haga click se llame a la funcion fetchAnswer()
+buttonLocation.addEventListener('click',()=>{
+    fetchAnswer();
+});
